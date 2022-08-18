31a2d5f22b69ca8d22de56146b6c262e21bfb1ed
krishnateja-akkapeddi <krishna.teja@goapptiv.com>
2022-08-18 16:10:54 +0530

    Story def

--- src/components/Button/GaButton.stories.tsx
+++ src/components/Button/GaButton.stories.tsx
@@ -17,7 +17,7 @@ const Template: ComponentStory<typeof GaButton> = (args) => (
 
 export const Basic = Template.bind({});
 Basic.args = {
-  children: "Hello qorld!",
+  children: "Hello orld!",
   color: ButtonColor.PRIMARY,
   size: ButtonSize.SMALL,
   variant: ButtonVariant.OUTLINED,
